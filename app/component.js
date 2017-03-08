02f6c7bfb50105c3418c65188df80dd3a4ba7061
Serge R. Kamel <skamel@borninteractive.com>
2017-03-08 08:30:44 +0200

    first commit

---
+++ app/component.js
@@ -0,0 +1,8 @@
+export default function (text = 'Hello World') {
+  const element = document.createElement('div');
+
+  element.className = 'pure-button';
+  element.innerHTML = text;
+
+  return element;
+}
---
+++ webpack.parts.js
@@ -0,0 +1,126 @@
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack');
+
+exports.devServer = function({ host, port } = {}) {
+  return {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      stats: 'errors-only',
+      host, // Defaults to `localhost`
+      port, // Defaults to 8080
+      overlay: {
+        errors: true,
+        warnings: true,
+      },
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  };
+};
+
+exports.lintJavaScript = function({ include, exclude, options }) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include,
+          exclude,
+          enforce: 'pre',
+
+          loader: 'eslint-loader',
+          options,
+        },
+      ],
+    },
+  };
+};
+
+exports.loadCSS = function({ include, exclude } = {}) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
+};
+
+exports.extractCSS = function({ include, exclude, use }) {
+  // Output extracted CSS to a file
+  const plugin = new ExtractTextPlugin({
+    filename: '[name].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+
+          use: plugin.extract({
+            use,
+            fallback: 'style-loader',
+          }),
+        },
+      ],
+    },
+    plugins: [ plugin ],
+  };
+};
+
+exports.autoprefix = function() {
+  return {
+    loader: 'postcss-loader',
+    options: {
+      plugins: () => ([
+        require('autoprefixer'),
+      ]),
+    },
+  };
+};
+
+exports.purifyCSS = function({ paths }) {
+  return {
+    plugins: [
+      new PurifyCSSPlugin({ paths: paths }),
+    ],
+  };
+};
+
+exports.lintCSS = function({ include, exclude }) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          include,
+          exclude,
+          enforce: 'pre',
+
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => ([
+              require('stylelint')({
+                // Ignore node_modules CSS
+                ignoreFiles: 'node_modules/**/*.css',
+              }),
+            ]),
+          },
+        },
+      ],
+    },
+  };
+};
